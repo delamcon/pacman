65635bdecdb8ac32175deafea6e962d09988e58e
aforghta <615anuta@gmail.com>
2021-01-17 21:19:33 +0300

    обновленное поле

--- main project.py
+++ main project.py
@@ -1,6 +1,6 @@
 import pygame
 
-WINDOW_SIZE = WIDTH, HEIGHT = 825,  575  # размер поля (33, 23), размер клетки 25
+WINDOW_SIZE = WIDTH, HEIGHT = 825,  560  # размер поля (33, 23), размер клетки 25
 
 
 class Board:
@@ -70,31 +70,5 @@ if __name__ == '__main__':
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 1 and not (start):
-                    xpos = (event.pos[0] - defx) // cell_size
-                    ypos = (event.pos[1] - defy) // cell_size
-                    if not ((0 > xpos or xpos >= 30) or (0 > ypos or ypos >= 30)):
-                        life.newcell((xpos, ypos))
-                        life.render()
-                        pygame.display.flip()
-            if event.type == pygame.KEYUP:
-                if event.key == 32:
-                    start = not (start)
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 3:
-                    start = not (start)
-            if event.type == MYEVENTTYPE and start:
-                life.next_move()
-                pygame.display.flip()
-            if event.type == pygame.MOUSEWHEEL and start:
-                if event.y == -1:
-                    if speed > 50:
-                        speed -= 25
-                        pygame.time.set_timer(MYEVENTTYPE, speed)
-                if event.y == 1:
-                    if speed < 1000:
-                        speed += 25
-                        pygame.time.set_timer(MYEVENTTYPE, speed)
         pygame.display.flip()
     pygame.quit()
