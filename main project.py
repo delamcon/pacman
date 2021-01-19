a8fe2c5f851668ceb6ed1007a56fec5575217d81
lewis <jec0bande1s0n@gmail.com>
2021-01-19 22:53:44 +0300

    неуверенные шаги

--- main project.py
+++ main project.py
@@ -1,13 +1,18 @@
 import pygame
 
 WINDOW_SIZE = WIDTH, HEIGHT = 475, 550  # размер поля (19, 22), размер клетки 25
-# оверка
+TICK = pygame.USEREVENT + 1  # событие, нужно для отсчета одного момента
+
 
 class Board:
     def __init__(self, screen):
         self.width = 19  # ширина поля
         self.height = 22  # высота поля
         self.screen = screen  # поверхность, на которой все выводим
+
+        self.y = 12  # координаты пакмана во вложенном списке
+        self.x = 9  # нужен для метода create_pacman в классе Pacman
+        self.PacmanCurrentPos = (225, 300)  # сохраняет позицию пакмана, (225, 300) - позиция в начале игры в пикселях
 
         self.left = 0  # отступ с левого верхнего края по оси x (пока нет счета очков, будет 0)
         self.top = 0  # отступ с левого верхнего края по оси y (пока нет счета очков, будет 0)
@@ -40,14 +45,8 @@ class Board:
         # 2 - стенка выхода привидений
 
     def render(self):
-        for x in range(self.width):
-            for y in range(self.height):
-
-                for i in range(-1, 2, 1):
-                    for j in range(-1, 2, 1):
-                        if (y + i >= 0 and x + j >= 0) and (y + i <= 21 and x + j <= 19):
-                            if self.board[y + i][x + j] == 0 and self.board[y][x] == 1:
-                                pygame.draw.line(self.screen, (0, 0, 128), start_pos, end_pos, width)
+        for y in range(self.height):
+            for x in range(self.width):
                 if self.board[y][x] == 0:
                     pygame.draw.rect(self.screen, (0, 0, 0), (x * self.cell_size + self.left,
                                                               y * self.cell_size + self.top,
@@ -63,7 +62,56 @@ class Board:
 
 
 class Pacman(Board):
-    pass
+    def create_pacman(self):  # создаем пакмана на поле, метод вызывается один раз
+        pygame.draw.rect(self.screen, (255, 255, 0), (self.x * self.cell_size + self.left,
+                                                       self.y * self.cell_size + self.top,
+                                                       self.cell_size, self.cell_size), width=0)
+
+    def pacman_move(self, key):
+        if key == 'a':
+            x = (self.PacmanCurrentPos[0] - 1 - self.left) // self.cell_size
+            y = (self.PacmanCurrentPos[1] - self.left) // self.cell_size
+            print(x, y, self.board[y][x], self.PacmanCurrentPos)
+            if self.board[y][x] != 1:
+                pygame.draw.rect(self.screen, (0, 0, 0), (self.PacmanCurrentPos[0], self.PacmanCurrentPos[1],
+                                                              self.cell_size, self.cell_size), width=0)
+                self.PacmanCurrentPos = (self.PacmanCurrentPos[0] - 1, self.PacmanCurrentPos[1])
+                pygame.draw.rect(self.screen, (255, 255, 0), (self.PacmanCurrentPos[0], self.PacmanCurrentPos[1],
+                                                              self.cell_size, self.cell_size), width=0)
+                pygame.display.flip()
+        if key == 's':
+            x = (self.PacmanCurrentPos[0] - self.left) // self.cell_size
+            y = (self.PacmanCurrentPos[1] + 1 - self.left) // self.cell_size
+            print(x, y, self.board[y][x], self.PacmanCurrentPos)
+            if self.board[y][x] != 1:
+                pygame.draw.rect(self.screen, (0, 0, 0), (self.PacmanCurrentPos[0], self.PacmanCurrentPos[1],
+                                                          self.cell_size, self.cell_size), width=0)
+                self.PacmanCurrentPos = (self.PacmanCurrentPos[0], self.PacmanCurrentPos[1] + 1)
+                pygame.draw.rect(self.screen, (255, 255, 0), (self.PacmanCurrentPos[0], self.PacmanCurrentPos[1],
+                                                              self.cell_size, self.cell_size), width=0)
+                pygame.display.flip()
+        if key == 'd':
+            x = (self.PacmanCurrentPos[0] + 1 - self.left) // self.cell_size
+            y = (self.PacmanCurrentPos[1] - self.left) // self.cell_size
+            print(x, y, self.board[y][x], self.PacmanCurrentPos)
+            if self.board[y][x] != 1:
+                pygame.draw.rect(self.screen, (0, 0, 0), (self.PacmanCurrentPos[0], self.PacmanCurrentPos[1],
+                                                          self.cell_size, self.cell_size), width=0)
+                self.PacmanCurrentPos = (self.PacmanCurrentPos[0] + 1, self.PacmanCurrentPos[1])
+                pygame.draw.rect(self.screen, (255, 255, 0), (self.PacmanCurrentPos[0], self.PacmanCurrentPos[1],
+                                                              self.cell_size, self.cell_size), width=0)
+                pygame.display.flip()
+        if key == 'w':
+            x = (self.PacmanCurrentPos[0] - self.left) // self.cell_size
+            y = (self.PacmanCurrentPos[1] - 1 - self.left) // self.cell_size
+            print(x, y, self.board[y][x], self.PacmanCurrentPos)
+            if self.board[y][x] != 1:
+                pygame.draw.rect(self.screen, (0, 0, 0), (self.PacmanCurrentPos[0], self.PacmanCurrentPos[1],
+                                                          self.cell_size, self.cell_size), width=0)
+                self.PacmanCurrentPos = (self.PacmanCurrentPos[0], self.PacmanCurrentPos[1] - 1)
+                pygame.draw.rect(self.screen, (255, 255, 0), (self.PacmanCurrentPos[0], self.PacmanCurrentPos[1],
+                                                              self.cell_size, self.cell_size), width=0)
+                pygame.display.flip()
 
 
 if __name__ == '__main__':
@@ -72,13 +120,34 @@ if __name__ == '__main__':
 
     running = True
 
+    pygame.time.set_timer(TICK, 20)  # отсчет по 75мс
+
     pacman = Pacman(screen)  # передаем только поверхность, потому что размеры известны
     pacman.render()
+    pacman.create_pacman()
     pygame.display.flip()
+    pacman.pacman_move('a')
 
+    PacmanCurrentKey = ''
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.KEYDOWN:  # проверка по кнопкам ASDW
+                if event.key == 97:  # проверяем A
+                    PacmanCurrentKey = 'a'
+                    pacman.pacman_move('a')
+                if event.key == 115:  # проверяем S
+                    PacmanCurrentKey = 's'
+                    pacman.pacman_move('s')
+                if event.key == 100:  # проверяем D
+                    PacmanCurrentKey = 'd'
+                    pacman.pacman_move('d')
+                if event.key == 119:  # проверяем W
+                    PacmanCurrentKey = 'w'
+                    pacman.pacman_move('w')
+            if event.type == TICK:
+                pacman.pacman_move(PacmanCurrentKey)
+
         pygame.display.flip()
     pygame.quit()
