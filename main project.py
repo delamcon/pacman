c515adfa520f70bf07b1aca5d2a0028fdb08cce4
aforghta <615anuta@gmail.com>
2021-01-20 00:03:20 +0300

    node list

--- main project.py
+++ main project.py
@@ -13,6 +13,8 @@ class Board:
         self.top = 0  # отступ с левого верхнего края по оси y (пока нет счета очков, будет 0)
         self.cell_size = 25  # размер клетки в пикселях
 
+        self.nodes_list = []
+
         self.board = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
                       [1, 3, 0, 0, 3, 0, 0, 0, 3, 1, 3, 0, 0, 0, 3, 0, 0, 3, 1],
                       [1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1],
@@ -58,6 +60,11 @@ class Board:
                     pygame.draw.rect(self.screen, (26, 185, 192), (x * self.cell_size + self.left,
                                                                    y * self.cell_size + self.top,
                                                                    self.cell_size, self.cell_size), width=0)
+                    self.nodes_list.append(3)
+
+        print(self.nodes_list)
+        print(len(self.nodes_list))
+
 
 
 class Pacman(Board):
